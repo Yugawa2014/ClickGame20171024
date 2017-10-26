8c7f17c7e5bb0c3d91f6ca1c3ba72c632263a37a
GT20176042\user <nkc.riku.yugawa@gmail.com>
2017-10-26 10:58:50 +0900

    現在の進行

---
+++ Assets/BallController.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallController : MonoBehaviour {
+    public void Shoot(Vector3 dir)
+    {
+        GetComponent<Rigidbody>().AddForce(dir);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        GetComponent<Rigidbody>().isKinematic = true;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //Shoot(new Vector3(0, 0, -50.0f));
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/BallGenerator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallGenerator : MonoBehaviour {
+
+    public GameObject BallPrefab;
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetMouseButtonDown(0))
+        {
+            GameObject Ball =
+                Instantiate(BallPrefab) as GameObject;
+            Ball.GetComponent<BallController>().Shoot(
+                new Vector3(0, 0, 50.0f));
+        }
+	}
+}
